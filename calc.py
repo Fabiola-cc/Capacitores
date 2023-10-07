44b6b2057c320908909817a48f6358a3ed3ba08f
Fabiola Contreras <fcontrerascol@gmail.com>
2023-10-06 18:19:05 -0600

    Corrección de errores

--- calc.py
+++ calc.py
@@ -7,27 +7,20 @@ Simulación de capacitancias
 Fabiola Contreras 22787
 María José Villafuerte 22129
 '''
-
-# Importamos las librerías necesarias para la simulación.
-from tkinter import DoubleVar
 import numpy as np # Libreria científica y matemática para Python.
-from scipy import constants, integrate   # Librerias para integrar funciones en un intervalo dado.
-'''
-Diccionario: 
-
-
-'''
 
 k = 8.988*10**9 #Valor de 1/4π∈₀
 e0= 8.85*10**(-12)
-#ecuacion_general_CampoE = (k*Q)/(r**2)
 
 '''Funciones de Capacitancia'''
 #C₀ = ∈₀*A/d
 def Capacitancia_PP(largo, ancho, separacion): #Placas Paralelas
     area = largo * ancho
-    division = area / separacion
-    return e0 * division
+    if separacion == 0:
+        return 0
+    else:
+        division = area / separacion
+        return e0 * division
 
 #C₀ = 4π∈₀*(rE*rI)/ln(rE/rI)
 def Capacitancia_E(radioE, radioI): #Esfera
@@ -41,14 +34,13 @@ def Capacitancia_C(largo, radioE, radioI): #Cilindro
     ln = 1/np.log(radioE/radioI)
     return inverso_k*largo*ln
 
-'''Funciones de Carga'''
+'''Función de Carga''' #C*V
 def Carga(capacitancia, voltaje):
     return capacitancia*voltaje
 
-'''Funciones de Energía'''
-#1/2(C)(V^2)
+'''Función de Energía''' #1/2(C)(V^2)
 def energia(capacitancia, voltaje):
-    return ((capacitancia/2)*(voltaje^2))
+    return ((capacitancia/2)*(voltaje**2))
 
 '''Funciones de Dieléctricos'''
 def dielectrico(capacitancia_inicial, constante_dielectrica):
@@ -56,7 +48,10 @@ def dielectrico(capacitancia_inicial, constante_dielectrica):
 
 def carga_libre_PP(Carga,largo, ancho):
     area = largo*ancho
-    return (Carga/area)
+    if area == 0:
+        return 0
+    else:
+        return (Carga/area)
 
 def carga_libre_Esfera(Carga, radio):
     area = 4*np.pi*(radio^2)
@@ -67,6 +62,4 @@ def carga_libre_Cilindro(Carga, largo, radioE):
     return (Carga/area)
 
 def carga_ligada(carga_libre, constante_dielectrica):
-    return carga_libre(1-(1/constante_dielectrica))
-
-
+    return carga_libre*(1-(1/constante_dielectrica))
--- main.py
+++ main.py
@@ -10,11 +10,10 @@ María José Villafuerte 22129
 
 import tkinter as tk # Libreria para creacion de interfaz grafica
 import calc
-import graficas
 
 def clear_frame():
     for widgets in Main_page.winfo_children():
-      widgets.destroy()
+        widgets.destroy()
 
 def main():
     clear_frame()
@@ -22,20 +21,21 @@ def main():
     tk.Label(Main_page, text = "Fabiola Contreras 22787\tMaría José Villafuerte 22129\n", font="Times 10 italic").pack()
     tk.Label(Main_page, text = "¿Qué tipo de capacitor deseas calcular?", font="Times 15 italic").pack()
 
-    tk.Label(Main_page, text = "\t Placas paralelas", font="Times 15").place(x=5, y=130)
+    tk.Label(Main_page, text = "\tPlacas paralelas", font="Times 15").place(x=5, y=130)
     tk.Button(text ="▷", command= calculo_PP).place(x=240, y=128)
 
-    tk.Label(Main_page, text = "\t Esférico", font="Times 15").place(x=5, y=160)
-    tk.Button(text ="▷", command = calculo_E).place(x=240, y=158)
+    tk.Label(Main_page, text = "\tEsférico", font="Times 15").place(x=5, y=160)
+    #tk.Button(text ="▷", command = calculo_E).place(x=240, y=158)
 
-    tk.Label(Main_page, text = "\t Cilíndrico", font="Times 15").place(x=5, y=190)
-    tk.Button(text ="▷", command = calculo_Cl).place(x=240, y=188)
+    tk.Label(Main_page, text = "\tCilíndrico", font="Times 15").place(x=5, y=190)
+    #tk.Button(text ="▷", command = calculo_Cl).place(x=240, y=188)
 
-def calculo_PP():
+def calculo_PP(): #Placas paralelas
     clear_frame()
-    tk.Label(Main_page, text = " \nPlacas Paralelas", font="Times 20").pack()
+    tk.Label(Main_page, text = "\n Capacitor de Placas Paralelas", font="Times 20").pack() #Titulo
 
-    tk.Label(Main_page, text = "Ingresa el largo de la placa", font="Times 8").place(x=10, y=60)
+    #----------------------------------Solicitud de datos-------------------------------------------------
+    tk.Label(Main_page, text = "Ingresa el largo de la placa", font="Times 8").place(x=10, y=60) 
     Input_Largo_placa = tk.DoubleVar()
     tk.Entry(textvariable=Input_Largo_placa).place(x=10,y=80)
 
@@ -47,111 +47,51 @@ def calculo_PP():
     Input_separacion_placa = tk.DoubleVar()
     tk.Entry(textvariable=Input_separacion_placa).place(x=10,y=160)
 
-    tk.Label(Main_page, text = "Ingresa el voltaje", font="Times 8").place(x=10, y=140)
+    tk.Label(Main_page, text = "Ingresa el voltaje", font="Times 8").place(x=10, y=180)
     Input_Voltaje_placa = tk.DoubleVar()
     tk.Entry(textvariable=Input_Voltaje_placa).place(x=10,y=200)
+    #------------------------------------------------------------------------------------------------------
 
-    def Call_calculation():
+    #----------------------------------Funciones de botones------------------------------------------------
+    def Call_capacitancia():
         capacitancia = calc.Capacitancia_PP(float(Input_Largo_placa.get()), float(Input_ancho_placa.get()), float(Input_separacion_placa.get()))
-       # graficas.Grafica_CampoE_LineasDeCarga(float(Input_Distancia.get()), E)
+        return capacitancia
 
     def Call_energia():
-        energia = calc.energia(float(Call_calculation), float(Input_Voltaje_placa.get()))
+        energia = calc.energia(float(Call_capacitancia()), float(Input_Voltaje_placa.get()))
+        return energia
 
     def Call_carga():
-        carga = calc.Carga(float(Call_calculation), float(Input_Voltaje_placa.get()))
+        carga = calc.Carga(float(Call_capacitancia()), float(Input_Voltaje_placa.get()))
+        return carga
 
     def Call_dec_vacio():
-        capacitancia_diec = calc.dielectrico(float(Call_calculation), 1)
+        capacitancia_diec = calc.dielectrico(float(Call_capacitancia()), 1)
         carga_libre = calc.carga_libre_PP(float(Call_carga()),float(Input_Largo_placa.get()), float(Input_ancho_placa.get()))
         carga_ligada = calc.carga_ligada(carga_libre, 1)
  
-
-    #NO RECUERDO QUE CONSTANTE USAR
     def Call_dec_mitad():
-        capacitancia_diec = calc.dielectrico(float(Call_calculation), 3.83)
+        capacitancia_diec = calc.dielectrico(float(Call_capacitancia()), 3.40)
         carga_libre = calc.carga_libre_PP(float(Call_carga()),float(Input_Largo_placa.get()), float(Input_ancho_placa.get()))
-        carga_ligada = calc.carga_ligada(carga_libre, 3.83)
- 
+        carga_ligada = calc.carga_ligada(carga_libre, 3.40)
+    #------------------------------------------------------------------------------------------------------
+
+    #----------------------------------Botones de resultados------------------------------------------------
     Clc_posibles=tk.Label(Main_page, text = "Cálculos:", font="Times 15 italic")
     Clc_posibles.place(x=15, y=230)
 
-    Boton_listo = tk.Button(text ="Capacitancia", command= Call_calculation)
-    Boton_listo.place(x=15, y=270)
-
-    Boton_listo = tk.Button(text ="Carga", command= Call_carga)
-    Boton_listo.place(x=15, y=300)
-
-    Boton_listo = tk.Button(text ="Energía", command= Call_energia)
-    Boton_listo.place(x=15, y=330)
-
-    Dielect=tk .Label(Main_page, text = "Dieléctricos:", font="Times 15 italic")
-    Dielect.place(x=200, y=250)
-
-    Boton_listo = tk.Button(text ="Vacio", command= Call_dec_vacio)
-    Boton_listo.place(x=200, y=280)
-
-    Boton_listo = tk.Button(text ="Medio lleno", command= Call_dec_mitad)
-    Boton_listo.place(x=200, y=310)
-
+    tk.Button(text ="Capacitancia", command= Call_capacitancia).place(x=15, y=270)
+    tk.Button(text ="Carga", command= Call_carga).place(x=15, y=300)
+    tk.Button(text ="Energía", command= Call_energia).place(x=15, y=330)
+    tk.Label(Main_page, text = "Dieléctricos:", font="Times 15 italic").place(x=200, y=250)
+    tk.Button(text ="Vacio", command= Call_dec_vacio).place(x=200, y=280)
+    tk.Button(text ="Medio lleno", command= Call_dec_mitad).place(x=200, y=310)
 
     Boton_atras = tk.Button(text ="Regresar", command= main)
     Boton_atras.place(x=10, y=10)
-    
-def calculo_E():
-    clear_frame()
-    tk.Label(Main_page, text = "\nAnillo de carga", font="Times 20").pack()
-
-    tk.Label(Main_page, text = "Ingresa el radio del anillo", font="Times 8").place(x=10, y=60)
-    Input_radio = tk.DoubleVar()
-    tk.Entry(textvariable=Input_radio).place(x=10,y=80)
-
-    tk.Label(Main_page, text = "Ingresa la carga del elemento", font="Times 8").place(x=10, y=100)
-    Input_Carga = tk.DoubleVar()
-    tk.Entry(textvariable=Input_Carga).place(x=10,y=120)
-
-    tk.Label(Main_page, text = "Ingresa la distancia 'x' entre la partícula y el anillo", font="Times 8").place(x=10, y=140)
-    Input_Distancia = tk.DoubleVar()
-    tk.Entry(textvariable=Input_Distancia).place(x=10,y=160)
-
-    def Call_calculation():
-        E = calc.Funcion_ecuacion_CampoE_anillo(float(Input_Carga.get()), float(Input_Distancia.get()), float(Input_radio.get()))
-        graficas.Grafica_CampoE_anillo(float(Input_radio.get()), float(Input_Distancia.get()), E)
-
-    Boton_listo = tk.Button(text ="Listo", command= Call_calculation)
-    Boton_listo.place(x=15, y=190)
-
-    Boton_atras = tk.Button(text ="Regresar", command= main)
-    Boton_atras.place(x=105, y=190)
-
-def calculo_Cl():
-    clear_frame()
-    tk.Label(Main_page, text = "\nDisco", font="Times 20").pack()
-    
-    tk.Label(Main_page, text = "Ingresa el radio del disco", font="Times 8").place(x=10, y=60)
-    Input_radio = tk.DoubleVar()
-    tk.Entry(textvariable=Input_radio).place(x=10,y=80)
-
-    tk.Label(Main_page, text = "Ingresa la carga del elemento", font="Times 8").place(x=10, y=100)
-    Input_Carga = tk.DoubleVar()
-    tk.Entry(textvariable=Input_Carga).place(x=10,y=120)
-
-    tk.Label(Main_page, text = "Ingresa la distancia 'x' entre la partícula y el disco", font="Times 8").place(x=10, y=140)
-    Input_Distancia = tk.DoubleVar()
-    tk.Entry(textvariable=Input_Distancia).place(x=10,y=160)
-
-    def Call_calculation():
-        E = calc.Funcion_ecuacion_CampoE_Disco(float(Input_Carga.get()), float(Input_Distancia.get()), float(Input_radio.get()))
-        graficas.Grafica_CampoE_Disco(float(Input_radio.get()), float(Input_Distancia.get()), E)
-
-    Boton_listo = tk.Button(text ="Listo", command= Call_calculation)
-    Boton_listo.place(x=15, y=190)
-
-    Boton_atras = tk.Button(text ="Regresar", command= main)
-    Boton_atras.place(x=105, y=190)
+    #-------------------------------------------------------------------------------------------------------
 
 Main_page = tk.Tk()
 Main_page.geometry("550x450")
 main()
 Main_page.mainloop()
-
